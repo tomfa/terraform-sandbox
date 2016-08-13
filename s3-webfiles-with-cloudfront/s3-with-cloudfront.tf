af07cc9481019bd7f0da3bc7bf7f316002d576cc
Tomas Fagerbekk <tomas.fagerbekk@bekk.no>
2016-08-13 11:25:13 +0200

    Add Cloudfront module

---
+++ s3-webfiles-with-cloudfront/s3-with-cloudfront.tf
@@ -0,0 +1,139 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+resource "aws_iam_user" "prod_user" {
+    name = "${var.bucket_name}-user"
+    path = "/system/"
+}
+
+resource "aws_iam_access_key" "prod_user" {
+    user = "${aws_iam_user.prod_user.name}"
+}
+
+resource "aws_iam_user_policy" "prod_user_ro" {
+    name = "prod"
+    user = "${aws_iam_user.prod_user.name}"
+   policy= <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::${var.bucket_name}",
+                "arn:aws:s3:::${var.bucket_name}/*"
+            ]
+        }
+   ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "prod" {
+    bucket = "${var.bucket_name}"
+    acl = "public-read"
+
+    website {
+        redirect_all_requests_to = "index.html"
+    }
+
+    cors_rule {
+        allowed_headers = ["*"]
+        allowed_methods = ["PUT","POST"]
+        allowed_origins = ["*"]
+        expose_headers = ["ETag"]
+        max_age_seconds = 3000
+    }
+
+    policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadForGetBucketObjects",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${var.bucket_name}/*"
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${aws_iam_user.prod_user.arn}"
+            },
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::${var.bucket_name}",
+                "arn:aws:s3:::${var.bucket_name}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_cloudfront_distribution" "prod_distribution" {
+    origin {
+        domain_name = "${aws_s3_bucket.prod.website_endpoint}"
+        origin_id   = "S3-${aws_s3_bucket.prod.bucket}"
+  
+        custom_origin_config  {
+            http_port = 80
+            https_port = 443
+            origin_protocol_policy = "match-viewer"
+            origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+        }
+    }
+    default_root_object = "index.html"
+    enabled             = true
+
+    custom_error_response {
+        error_caching_min_ttl = 3000
+        error_code            = 404
+        response_code         = 200
+        response_page_path    = "/index.html"
+    }
+
+    default_cache_behavior {
+        allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods   = ["GET", "HEAD"]
+        target_origin_id = "S3-${aws_s3_bucket.prod.bucket}"
+
+        forwarded_values {
+            query_string = true
+  
+            cookies {
+                forward = "all"
+            }
+            headers = ["*"]
+      }
+
+        viewer_protocol_policy = "allow-all"
+        min_ttl                = 0
+        default_ttl            = 3600
+        max_ttl                = 86400
+    }
+
+    # Distributes content to US and Europe
+    price_class = "PriceClass_100"
+
+    # Restricts who is able to access this content
+    restrictions {
+        geo_restriction {
+            # type of restriction, blacklist, whitelist or none
+            restriction_type = "none"
+        }
+    }
+
+    # SSL certificate for the service.
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+}
---
+++ s3-webfiles-with-cloudfront/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "bucket_name" {}
