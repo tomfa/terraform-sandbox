0fbce36000a5347beb8de92fe6d1b711df1136e2
Tomas Fagerbekk <tomas@webutvikling.org>
2016-11-12 15:35:05 +0100

    Add description for variables

--- s3-double-webfile-bucket/variables.tf
+++ s3-double-webfile-bucket/variables.tf
@@ -1,7 +1,14 @@
-variable "aws_region" {}
+variable "aws_region" {
+	description = "e.g. eu-central-1"
+}
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+	description = "e.g. AKIA..."
+}
+variable "aws_secret_key" {
+	description = "e.g. yaAS$1..."
+}
 
-variable "prod_bucket_name" {}
-variable "test_bucket_name" {}
+variable "bucket_name" {
+	description = "e.g. your-bucket-name"
+}
--- s3-double-webfile-with-cloudfront/variables.tf
+++ s3-double-webfile-with-cloudfront/variables.tf
@@ -1,6 +1,14 @@
-variable "aws_region" {}
+variable "aws_region" {
+	description = "e.g. eu-central-1"
+}
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+	description = "e.g. AKIA..."
+}
+variable "aws_secret_key" {
+	description = "e.g. yaAS$1..."
+}
 
-variable "bucket_name" {}
+variable "bucket_name" {
+	description = "e.g. your-bucket-name"
+}
